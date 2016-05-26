911644fe49eccdf18a959d4df8ddc546c8249372
Vlad <vladnaiden@yahoo.com>
2016-05-26 23:19:33 +0300

    asdgsd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');asdasdasdasdasdasd
+app.set('view engine', 'ejs');asdasdasdasdasasdasddasd
 asdasdasdasdsfscvxcv xcv
 x x svsdfsdfsd
 app.set('views', path.join(__dirname, 'views'));
